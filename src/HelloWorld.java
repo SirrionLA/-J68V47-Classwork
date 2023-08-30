d9ab49d551f1b26672e3c3482c95a432d34b1fe3
Edvin <30055333@nescol.ac.uk>
2023-08-30 11:27:48 +0100

    First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * My first program
+ * @author Edvin Lastovskij
+ * @version 1
+ */
+
+public class HelloWorld {
+
+    public static void main (String[] args) {
+        // Write out Hello World to the console
+        System.out.println("Hello World");
+    }
+}
