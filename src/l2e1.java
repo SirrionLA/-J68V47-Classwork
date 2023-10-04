b9925b0e8c476574d4c7798ab9579657f818ce54
Edvin <30055333@nescol.ac.uk>
2023-10-04 11:31:14 +0100

    First Commit

---
+++ src/l2e1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class l2e1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name ");
+        String name = input.nextLine();
+        System.out.print("Enter your hobby ");
+        String hobby = input.nextLine();
+        System.out.print(name+" "+"loves"+" "+hobby+" "+"during his free time.\nIt helps him to relax and recover from stress.");
+
+    }
+}
