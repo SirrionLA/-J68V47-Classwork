808ae8955b327411bc843368c68bed488bcbf75a
Edvin <30055333@nescol.ac.uk>
2023-10-04 11:03:32 +0100

    First Commit

---
+++ src/l1e2.java
@@ -0,0 +1,9 @@
+public class l1e2 {
+    public static void main(String[] args) {
+        System.out.println("CPU metro = computer");
+        System.out.println("art radios = radiators");
+        System.out.println("aging pack = packaging");
+        System.out.println("ceo jibe TV = objective");
+        System.out.println("bar thinly = labyrinth");
+    }
+}
