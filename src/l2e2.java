d1b9f30ea5fbabc03a264e33c9a626fec2816cf4
Edvin <30055333@nescol.ac.uk>
2023-10-04 11:54:33 +0100

    First Commit

---
+++ src/l2e2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class l2e2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the first number? ");
+        String number1 = input.nextLine();
+        System.out.print("What is the second number? ");
+        String number2 = input.nextLine();
+
+
+    }
+}
